f44940eb87a0bf9143422fe847674562f96b2e73
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-22 20:32:01 +0700

    push nomor 3 script-3

--- frontend/javascript/3-comparison-cp/script-3.js
+++ frontend/javascript/3-comparison-cp/script-3.js
@@ -6,6 +6,26 @@
 // E = <= 50
 
 // Masukan suatu angka
-const score = parseInt(prompt("Masukan nilai: "));
+// const score = parseInt(prompt("Masukan nilai: "));
 
 // TODO: answer here
+
+const prompt = require("prompt-sync")();
+let nilai = parseInt(prompt("Masukan nilai: "));
+let huruf;
+
+switch (true) {
+    case nilai >= 91 && nilai <= 100 : huruf = "A";
+        break;
+    case nilai >= 71 && nilai <= 90 : huruf = "B";
+        break;
+    case nilai >= 61 && nilai <= 70 : huruf = "C";
+        break;
+    case nilai >= 51 && nilai <= 60 : huruf = "D";
+        break;
+    case nilai >= 0 && nilai <= 50 : huruf = "E";
+        break;
+    default:
+        huruf = "tidak ada";
+}
+console.log(huruf);
