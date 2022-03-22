2ea782e2216a496cc16b02eda9cb0931a1d87517
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-22 20:31:02 +0700

    push nomor 3 script-1

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -1,6 +1,19 @@
 // Pengecekan angka positif, negatif, atau 0 dengan nested if
 
 // Masukan suatu angka
-const number = parseInt(prompt("Masukan bilangan: "));
+// const x = parseInt(prompt("Masukan bilangan: "));
 
 // TODO: answer here
+
+const prompt = require("prompt-sync")();
+let x = parseInt(prompt('Masukkan bilangan : '));
+
+if (x >= 0) {
+    if (x == 0) {
+        console.log("bilangan nol");
+    } else {
+        console.log("bilangan negatif");
+    }
+} else {
+    console.log("bilangan positif");
+}
