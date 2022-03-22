06bf7a4eccc4792ac0d8fc1c0f2161c80d802db5
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-22 20:26:01 +0700

    push nomor 2

--- frontend/javascript/2-variable-cp/script.js
+++ frontend/javascript/2-variable-cp/script.js
@@ -1,5 +1,4 @@
 // Menukar variabel a dan b dengan operator matematika
-// * ALGORITMA
 const prompt = require("prompt-sync")();
 let a = prompt('Masukan variabel pertama: ');
 let b = prompt('Masukan variabel kedua: ');
@@ -9,12 +8,3 @@ let b = prompt('Masukan variabel kedua: ');
 
 console.log(`Nilai a setelah ditukar: ${a}`);
 console.log(`Nilai b setelah ditukar: ${b}`);
-
-// * PSEUDOCODE
-// * LET a DISPLAY('Masukan variabel pertama: ')
-// * LET b DISPLAY('Masukan variabel pertama: ')
-
-// * SWAP A,B TO B,A
-
-// * DISPLAY(`Nilai a setelah ditukar: ${a}`)
-// * DISPLAY(`Nilai b setelah ditukar: ${b}`)
