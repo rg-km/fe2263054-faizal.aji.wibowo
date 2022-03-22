b21d71e6bf498b76646a5b8f98c56da7b63571bb
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-22 20:31:24 +0700

    push nomor 3 script-2

--- frontend/javascript/3-comparison-cp/script-2.js
+++ frontend/javascript/3-comparison-cp/script-2.js
@@ -1,9 +1,10 @@
 // Masukan 3 bilangan bulat menggunakan if condition
+const prompt = require("prompt-sync")();
 const num1 = parseInt(prompt("Masukan bilangan pertama: "));
 const num2 = parseInt(prompt("Masukan bilangan kedua: "));
 const num3 = parseInt(prompt("Masukan bilangan ketiga: "));
 
-let largest;
+let largest = Math.max(num1, num2, num3);
 
 // TODO: answer here
 
