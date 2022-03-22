cc76f4995740f3890beec90f8cd0797520bc6b4e
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-22 19:35:37 +0700

    push nomor 1 exercise 2

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
@@ -2,3 +2,21 @@
 // Seseorang diperbolehkan membuat SIM jika usianya lebih besar atau sama dengan 17 tahun.
 
 // TODO: answer here
+// * ALGORITMA
+const prompt = require("prompt-sync")();
+let usia = parseInt(prompt('Masukkan: '));
+
+if (usia >= 17) {
+    console.log("Umur "+ usia + " tahun" +" bisa membuat SIM");
+} else {
+    console.log("Umur "+ usia + " tahun" +" tidak bisa membuat SIM");
+}
+
+// * PSEUDOCODE
+
+// * VARIABLE usia;
+// * DISPLAY ("Masukkan Usia: ");
+// * IF (usia >= 17)
+// 	*	  	DISPLAY ("Umur "+ usia + " tahun" +" bisa membuat SIM");
+// 	*  ELSE
+// 	*		DISPLAY ("Umur "+ usia + " tahun" +" tidak bisa membuat SIM");
--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,30 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+// * ALGORITMA
+let angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let jumlah = 0;
+let genap = [];
+
+for (let i = 0; i < angka.length; i++) {
+    if(angka[i] % 2 == 0){
+        genap.push(angka[i]); 
+        jumlah += angka[i];
+    }
+}
+console.log(genap);
+console.log(jumlah);
+
+// * PSEUDOCODE
+
+// * VARIABLE angka ARRAY 1 TO 10 
+// * VARIABLE jumlah 0
+// * VARIABLE genap []
+
+// * FOR LOOP (let i = 0; i < angka.length; i++)
+// *    IF angka[i] % 2 == 0
+// *       VARIABLE genap PUSH TO ARRAY angka
+// *       VARIABLE jumlah SUM WITH ARRAY angka
+
+// * DISPLAY VARIABLE genap
+// * DISPLAY VARIABLE jumlah
