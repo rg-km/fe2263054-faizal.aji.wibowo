5c5f3f4891a870e573a49ac61fb4ef944edccd41
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-23 14:28:07 +0700

    push nomor 4 soal 1

--- frontend/javascript/4-looping-cp/script-1.js
+++ frontend/javascript/4-looping-cp/script-1.js
@@ -12,6 +12,26 @@
 
 
 // Masukan jumlah bilangan yang ingin dicek, iterasi dari angka 1
-const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
+// const prompt = require("prompt-sync")();
+// const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
 
 // TODO: answer here
+const prompt = require("prompt-sync")();
+const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
+
+function kelipatan(counter){
+    let hasil = '';
+    for(let i=1; i<=counter; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+        hasil = counter + ' merupakan kelipatan 3 dan 5';
+      } else if(i % 3 == 0) {
+        hasil = counter + ' merupakan kelipatan 3';
+      } else if(i % 5 == 0) {
+        hasil = counter + ' merupakan kelipatan 5';
+      } else {
+        hasil = counter + ' bukan merupakan kelipatan 3 ataupun 5';
+        }
+    }
+    return hasil;
+}
+console.log(kelipatan(counter));
