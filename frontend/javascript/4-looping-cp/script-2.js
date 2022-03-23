11a7e3da4f4a3b9b7788255df06d7d7557717aa0
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-23 14:28:43 +0700

    push nomor 4 soal 2

--- frontend/javascript/4-looping-cp/script-2.js
+++ frontend/javascript/4-looping-cp/script-2.js
@@ -10,6 +10,18 @@
 // *
 
 // Masukan jumlah baris
-const n = parseInt(prompt("Masukan jumlah baris: "));
+// const n = parseInt(prompt("Masukan jumlah baris: "));
 
 // TODO: answer here
+const prompt = require("prompt-sync")();
+const n = parseInt(prompt("Masukan jumlah baris: "));
+
+    let hasil = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i; j++) {
+            hasil += "*";
+        }
+        hasil += "\n";
+        
+    }
+console.log(hasil);
