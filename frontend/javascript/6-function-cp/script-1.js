dc2091e8437eef3a54ba7e3c99ff0b8f11d6e6a0
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-23 15:39:11 +0700

    push nomor 6 soal 1

--- frontend/javascript/6-function-cp/script-1.js
+++ frontend/javascript/6-function-cp/script-1.js
@@ -4,12 +4,17 @@
 // teks = hello world
 // hasil:
 // dlrow olleh
+const prompt = require("prompt-sync")();
+// take input from the user
+const string = prompt('Masukan teks: ');
+const result = reverseString(string);
 
 function reverseString(str) {
     // TODO: answer here
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
-
-const string = prompt('Masukan teks: ');
-
-const result = reverseString(string);
 console.log(result);
