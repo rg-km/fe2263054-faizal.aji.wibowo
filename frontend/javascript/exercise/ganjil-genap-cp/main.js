9f03e26b79f964d1eef34f7916652ebe066e8632
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-29 13:28:51 +0700

    push ganjil-genap-cp

--- frontend/javascript/exercise/ganjil-genap-cp/main.js
+++ frontend/javascript/exercise/ganjil-genap-cp/main.js
@@ -10,12 +10,41 @@ Setiap plat nomer di pisahkan oleh karakter ";"
 
 function ganjilGenap(plat) {
   // TODO: answer here
+  if (plat === undefined) {
+    return "invalid data";
+
+  } else if (plat === "") {
+    return "plat tidak ditemukan";
+
+  } else {
+    let platGanjil = 0;
+    let platGenap = 0;
+    let platStr = plat.split(";");
+
+    for (let i = 0; i < platStr.length; i++) {
+      if (platStr[i] % 2 === 0) {
+        platGenap++;
+      } else {
+        platGanjil++;
+      }
+    }
+
+    if (platGanjil === 0 && platGenap === 0) {
+      return "plat ganjil tidak ditemukan dan plat genap tidak ditemukan";
+    } else if (platGanjil === 0) {
+      return "plat genap sebanyak " + platGenap + " dan plat ganjil tidak ditemukan";
+    } else if (platGenap === 0) {
+      return "plat ganjil sebanyak " + platGanjil + " dan plat genap tidak ditemukan";
+    } else {
+      return "plat genap sebanyak " + platGenap + " dan plat ganjil sebanyak " + platGanjil;
+    }
+  }
 }
 
-console.log(ganjilGenap('2341;3429;864;1309;1276')) //plat genap sebanyak 2 dan plat ganjil sebanyak 3
-console.log(ganjilGenap('2347;3429;1305')) //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
-console.log(ganjilGenap('864;1308;1276;1432')) //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
-console.log(ganjilGenap('')) //plat tidak ditemukan
-console.log(ganjilGenap()) //invalid data
+console.log(ganjilGenap("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
+console.log(ganjilGenap("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
+console.log(ganjilGenap("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
+console.log(ganjilGenap("")); //plat tidak ditemukan
+console.log(ganjilGenap()); //invalid data
 
-module.exports = ganjilGenap
+module.exports = ganjilGenap;
