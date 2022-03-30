7b09a763b39515123f090b3dbd84b64e52e7ed0f
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-30 22:33:23 +0700

    push compare-the-triplets

--- frontend/javascript/hackerrank/compare-the-triplets/main.js
+++ frontend/javascript/hackerrank/compare-the-triplets/main.js
@@ -17,6 +17,17 @@
 function compareTriplets(a, b) {
   // Write your code here
   // TODO: answer here
+  let aliceScore = 0
+  let bobScore = 0
+  
+  for (let i=0; i<a.length; i++){
+      if (a[i] > b[i]){
+          aliceScore++
+      } else if(a[i] < b[i]){
+          bobScore++
+      }
+  }
+  return [aliceScore, bobScore]
 }
 function main() {
 
