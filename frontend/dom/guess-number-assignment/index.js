1cc23f192886b1bec83454943ef047501fa9643f
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-06 06:04:03 +0700

    push guess-number

--- frontend/dom/guess-number-assignment/index.js
+++ frontend/dom/guess-number-assignment/index.js
@@ -11,14 +11,12 @@ Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama 
 // let score = nilai yang akan ditampilakan
 
 // TODO: answer here
-const displayScore = document.querySelector('.score');
-let score = 10;
-
-const input = document.querySelector('.input');
-
-const message = document.querySelector('.message');
-
 let randomNumber = Math.trunc(Math.random() * 10) + 1;
+let message = document.querySelector('.message');
+let displayScore = document.querySelector('.number');
+let input = document.querySelector('.input');
+let score = document.querySelector('.score');
+score.innerHTML = 10;
 
 let checkBtn = document.querySelector('.check');
 checkBtn.addEventListener('click', checkNumber);
@@ -28,17 +26,15 @@ newGameBtn.addEventListener('click', resetGame)
 
 function displayMessage(msg) {
   // TODO: answer here
-
+  message.innerHTML = msg;
 }
 
 function resetGame() {
   // TODO: answer here
-  score = 10;
-  randomNumber = Math.trunc(Math.random() * 10) + 1;
-  displayScore.textContent = 'Ayo tebak...';
-  document.querySelector('.number').textContent = '?';
-  score.textContent = score;
+  displayScore.innerHTML = "?";
   input.value = '';
+  message.innerHTML = 'Lets start guessing...';
+  score.innerHTML = 10;
   //dilarang menghapus code dibawah ini!
   document.getElementById('hidden-number').innerHTML = randomNumber
 }
@@ -46,35 +42,22 @@ function resetGame() {
 function checkNumber() {
   // TODO: answer here
   // kalo inputnya kurang dari 1
-  if(input.value < 1 || input.value > 10) {
-    message.textContent = 'Tebak angka diantara 1 sampai 10';
-
-    // kalo inputnya sama dengan randomNumber
-  } else if(input.value == randomNumber){
-    document.querySelector('.number').textContent = randomNumber;
-    message.textContent = 'Wow you got it man!';
-
-    // kalo inputnya lebih tinggi dari randomNumber
-  } else if(input.value > randomNumber){
-    if (score < 1){
-      message.textContent = 'Sorry, Kamu kalah';
-    } else{
-      message.textContent = 'Opps terlalu tinggi';
-      score--;
-      displayScore.textContent = score;
+  if(score.innerHTML > 0){
+    if(input.value === '' || input.value == 0 || input.value < 0 || input.value > 10){
+      displayMessage("Guess any number between 1 and 10");
+    } else if(input.value == randomNumber) {
+      displayMessage('You are correct!');
+      displayScore.innerHTML = randomNumber;
+    } else if(input.value > randomNumber) {
+      displayMessage("Oww... that's too big!");
+      score.innerHTML--;
+    } else if(input.value < randomNumber) {
+      displayMessage("Too small, buddy!");
+      score.innerHTML--;
     }
-
-    // kalo inputnya kurang dari randomNumber
-  } else if (input.value < randomNumber){
-    if(score < 1){
-      message.textContent = 'Sorry, Kamu kalah';
-    } else{
-      message.textContent = 'Opps terlalu rendah';
-      score--;
-      displayScore.textContent = score;
+  } if (score.innerHTML == 0) {
+      displayMessage('You lose!');
     }
-  }
-
 }
 
 //dilarang menghapus code dibawah ini!
