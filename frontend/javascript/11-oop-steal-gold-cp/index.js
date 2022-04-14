a9e7b264233c227b31ab29504f03a1c48358bdf6
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-14 10:50:19 +0700

    fix steal-gold-cp

--- frontend/javascript/11-oop-steal-gold-cp/index.js
+++ frontend/javascript/11-oop-steal-gold-cp/index.js
@@ -6,35 +6,34 @@ class Player {
     this.hasBeenRobbed = false;
   }
 
-
   getGold() {
-  // TODO: answer here
-  return this.gold;
+    // TODO: answer here
+    return this.gold;
   }
 
   setGold(gold) {
-  // TODO: answer here
-  this.gold = gold;
+    // TODO: answer here
+    this.gold = gold;
   }
 
   getStealChance() {
-  // TODO: answer here
-  this.stealChance = this.randomizer();
+    // TODO: answer here
+    return this.stealChance;
   }
 
   setStealChance(chance) {
-  // TODO: answer here
-  this.stealChance = chance;
+    // TODO: answer here
+    this.stealChance = chance;
   }
 
   getHasBeenRobbed() {
     return this.hasBeenRobbed;
   }
-  
+
   setHasBeenRobbed(status) {
     this.hasBeenRobbed = status;
   }
-  
+
   randomizer() {
     return +Math.random().toFixed(2);
   }
@@ -44,15 +43,18 @@ class Player {
   //  - Jika gold lawan lebih kecil dari 5 gold, steal gagal dengan message "Lawan terlalu miskin"
   steal(player) {
     // TODO: answer here
-    if (this.stealChance > player.randomizer() || player.getGold() < 5) {
-      if (player.getGold() < 5) {
-        return "Lawan terlalu miskin";
-      } else {
-        player.setGold(this.getGold() - 5);
-        this.setGold(this.getGold() + 5);
-        return "Berhasil mencuri 5 gold";
-      }
-    } else {
+    if (this.randomizer() <= this.stealChance) {
+      this.hasBeenRobbed = true;
+      this.setGold(this.gold + 5);
+      player.setGold(player.gold - 5);
+      return "Berhasil mencuri 5 gold";
+    }
+    if (player.gold < 5) {
+      this.hasBeenRobbed = false;
+      return "Lawan terlalu miskin";
+    }
+    if (this.randomizer() > this.stealChance) {
+      this.hasBeenRobbed = false;
       return "Gagal mencuri, coba lain kali";
     }
   }
@@ -61,10 +63,10 @@ class Player {
 const p1 = new Player("Fauzan");
 const p2 = new Player("Tegar");
 
-console.log('Jumlah gold Player 1: ' + p1.getGold())
-console.log('Jumlah gold Player 2: ' + p2.getGold())
+console.log("Jumlah gold Player 1: " + p1.getGold());
+console.log("Jumlah gold Player 2: " + p2.getGold());
 console.log(p1.steal(p2));
-console.log('Total gold Player 1: ' + p1.getGold())
-console.log('Total gold Player 2: ' + p2.getGold())
+console.log("Total gold Player 1: " + p1.getGold());
+console.log("Total gold Player 2: " + p2.getGold());
 
 module.exports = Player;
