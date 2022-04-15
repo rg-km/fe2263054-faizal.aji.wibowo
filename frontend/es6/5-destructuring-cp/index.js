4455b1ab304877cd6af703385ed4703662847c3d
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-15 20:10:48 +0700

    push destructuring-cp

--- frontend/es6/5-destructuring-cp/index.js
+++ frontend/es6/5-destructuring-cp/index.js
@@ -2,6 +2,8 @@
 
 const format = (user) => {
   // TODO: answer here
+  let {name, email} = user;
+  return `${name} - ${email}`;
 };
 
 console.log(format({ name: "John", email: "john@example.com" }))
