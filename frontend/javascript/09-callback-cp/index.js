e93ef4d097c52bec191412ee5d7bea2ae22d7fa9
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-18 22:46:19 +0700

    push callback-cp

--- frontend/javascript/09-callback-cp/index.js
+++ frontend/javascript/09-callback-cp/index.js
@@ -4,11 +4,27 @@
 // contoh: hehe => hheehhee
 function doubleChars(str) {
   // TODO: answer here
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + str[i];
+  }
+  return result;
+  
+  // cara lain:
+  // return str.split("").map((char) => char + char).join("");
 }
 
 // function ini mengulang pengaplikasian callback pada str sejumlah num
 function repeat(str, num, cb) {
   // TODO: answer here
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += cb(str[i]).repeat(num);
+  }
+  return result;
+  
+  // cara lain:
+  // return cb(str).split("").map((char) => char.repeat(num)).join("");
 }
 
 console.log(repeat("triple", 2, doubleChars)); //  ttttrrrriiiipppplllleeee
