ee39e57039531e8f776170d178c29be1f77b4016
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-21 09:55:49 +0700

    push sort-cp

--- frontend/data-structure/array/1-sort-cp/index.js
+++ frontend/data-structure/array/1-sort-cp/index.js
@@ -13,11 +13,27 @@
 // [1,2,3,4,5] -> [2,3,4,5,1] -> [3,4,5,1,2] -> [4,5,1,2,3] -> [5,1,2,3,4]
 
 function sort(arr) {
-    return arr // TODO: replace this
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 function rotateLeft(d, arr) {
-    return arr // TODO: replace this
+    for (let i = 0; i < d; i++) {
+        let temp = arr[0];
+        for (let j = 0; j < arr.length - 1; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[arr.length - 1] = temp;
+    }
+    return arr;
 }
 
 let nums = [4, 5, 2, 1, 3]
