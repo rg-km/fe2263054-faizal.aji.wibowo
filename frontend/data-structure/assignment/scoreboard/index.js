67419d11eca4a979c966e65689636cfc8bfd3a05
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-21 16:17:06 +0700

    assignment

--- frontend/data-structure/assignment/scoreboard/index.js
+++ frontend/data-structure/assignment/scoreboard/index.js
@@ -16,6 +16,27 @@ class Scores {
 
   topStudents() {
     // TODO: answer here
+    let result = [];
+    let scores = this.scores;
+    scores.sort((a, b) => {
+      if (a.correct > b.correct) {
+        return -1;
+      } else if (a.correct < b.correct) {
+        return 1;
+      } else {
+        if (a.wrong < b.wrong) {
+          return -1;
+        } else if (a.wrong > b.wrong) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
+    for (let i = 0; i < scores.length; i++) {
+      result.push(scores[i].name);
+    }
+    return result;
   }
 }
 
