456a356468dd0edc64c740ec9e2593d938aaacd5
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-25 09:32:32 +0700

    push hashmap-cp

--- frontend/data-structure/hashmap/1-hashmap-cp/index.js
+++ frontend/data-structure/hashmap/1-hashmap-cp/index.js
@@ -1,8 +1,8 @@
 function ageDistribution(persons) {
   let result = new Map();
   // TODO: answer here
-  for (let person of persons) {
-    let age = person.age;
+  for (let i = 0; i < persons.length; i++) {
+    let age = persons[i].age;
     if (result.has(age)) {
       result.set(age, result.get(age) + 1);
     } else {
@@ -15,12 +15,12 @@ function ageDistribution(persons) {
 function groupByAge(persons) {
   let result = new Map();
   // TODO: answer here
-  for (let person of persons) {
-    let age = person.age;
+  for (let i = 0; i < persons.length; i++) {
+    let age = persons[i].age;
     if (result.has(age)) {
-      result.get(age).push(person);
+      result.get(age).push(persons[i]);
     } else {
-      result.set(age, [person]);
+      result.set(age, [persons[i]]);
     }
   }
   return result;
