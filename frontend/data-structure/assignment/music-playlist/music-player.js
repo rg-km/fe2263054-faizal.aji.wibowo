ca5f36f40a03d1d1f58ff9c9cb31d04dba6ea97a
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-26 23:39:08 +0700

    push assignment

--- frontend/data-structure/assignment/music-playlist/music-player.js
+++ frontend/data-structure/assignment/music-playlist/music-player.js
@@ -1,16 +1,29 @@
-const Song = require('./song')
-const Playlist = require('./playlist')
+const Song = require("./song");
+const Playlist = require("./playlist");
 
 module.exports = class MusicPlayer {
-    constructor(playlist) {
-        this.playlist = playlist
+  constructor(playlist) {
+    this.playlist = playlist;
+  }
+
+  addSong(song) {
+    // TODO - answer here
+    this.playlist.songs.push(song);
+  }
+
+  play() {
+    // TODO - answer here
+    let temp = [];
+    for (let i = 0; i < this.playlist.songs.length; i++) {
+        temp.push(this.playlist.songs[i].play());
     }
 
-    addSong(song) {
-        // TODO: answer here
+    if (this.playlist.songs.length === 0) {
+      return "";
+    } else if (this.playlist.songs.length === 1) {
+      return (this.playlist.songs[0].singer + " - " + this.playlist.songs[0].title);
+    } else if (this.playlist.songs.length === 2) {
+      return (this.playlist.songs[0].singer + " - " + this.playlist.songs[0].title + " " + this.playlist.songs[1].singer + " - " + this.playlist.songs[1].title);
     }
-
-    play() {
-        // TODO: answer here
-    }
-}
+  }
+};
