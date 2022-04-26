58f540f884b6541f9a3e6666b33d8a2c1d36b1b3
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-26 08:40:21 +0700

    push set-cp

--- frontend/data-structure/set/1-set-cp/index.js
+++ frontend/data-structure/set/1-set-cp/index.js
@@ -14,7 +14,12 @@
 
 function intersection(setA, setB) {
     let result = new Set();
-    return result; // TODO: replace this
+    for (const array of setA) {
+        if (setB.has(array)) {
+            result.add(array);
+        }
+    }
+    return result; 
 }
 
 let setA = new Set(["Java", "Python", "Javascript", "C ++", "C#"]);
