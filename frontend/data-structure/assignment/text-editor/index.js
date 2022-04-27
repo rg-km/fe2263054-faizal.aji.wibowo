2c8af1a3cca7c0225a111197ace26e8854cec894
faizalajiw <faizalajiwibowo7@gmail.com>
2022-04-27 23:01:41 +0700

    push assignment

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,64 @@
 module.exports = class TextEditor {
-    constructor() {
-        // TODO: answer here
-    }
+  constructor() {
+    // TODO: answer here
+    this.stack = [];
+    this.undoStack = [];
+    this.redoStack = [];
+    this.top = -1;
+    this.size = 5;
+  }
 
-    write(c) {
-        // TODO: answer here
-    }
+  write(c) {
+    // TODO: answer here
+    this.stack.push(c);
+    this.top++;
+    this.undoStack.push(c);
+  }
 
-    read() {
-        // TODO: answer here
-    }
+  read() {
+    // TODO: answer here
+    let peek = "";
 
-    undo() {
-        // TODO: answer here
+    this.stack.forEach((c) => {
+      peek += c;
+    });
+
+    if (peek.length === "") {
+      return "";
+    } else {
+      console.log(peek);
     }
-    
-    redo() {
-        // TODO: answer here
+    return peek;
+  }
+
+  undo() {
+    // TODO: answer here
+    if (this.undoStack.length === 0) {
+      return "";
+    } else {
+      // hapus elemen terakhir dari undoStack
+      let lastUndo = this.undoStack.pop();
+      // push ke redo stack
+      this.redoStack.push(lastUndo);
+      // hapus elemen terakhir dari stack
+      this.top--;
+      this.stack.pop();
     }
-}
+  }
+
+  redo() {
+    // TODO: answer here
+    if (this.redoStack.length === 0) {
+      return "";
+    } else {
+      // hapus elemen terakhir dari redoStack
+      let lastRedo = this.redoStack.pop();
+      // push ke undo stack
+      this.undoStack.push(lastRedo);
+      // push ke undo stack
+      this.top++;
+      // push ke stack
+      this.stack.push(lastRedo);
+    }
+  }
+};
