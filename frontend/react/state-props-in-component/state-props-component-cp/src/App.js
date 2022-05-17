3f92688da82b617a57c374d9eea5b6d796b44306
faizalajiw <faizalajiwibowo7@gmail.com>
2022-05-18 00:30:16 +0700

    props-cp

--- frontend/react/state-props-in-component/state-props-component-cp/src/App.js
+++ frontend/react/state-props-in-component/state-props-component-cp/src/App.js
@@ -1,74 +1,96 @@
-import React from 'react';
+import React from "react";
 
 export const Item = (props) => {
   // TODO: answer here
+  const { id, image, title } = props;
   const [item, setItem] = React.useState(0);
 
-  return(
-    <div className='each-box'>
-      <img src={image} alt="each-item"/>
+  return (
+    <div className="each-box">
+      <img src={image} alt="each-item" />
       <p>{title}</p>
       <div>
         <button
-        aria-label={`minus-button-${id}`}
-        onClick={()=>{
-        // TODO: answer here
-        }}
-        >-</button>
-        <input type="text" aria-label={`item-${id}`} className={`item-${id}`} value={item} disabled></input>
+          aria-label={`minus-button-${id}`}
+          onClick={() => {
+            // TODO: answer here
+            setItem(item - 1);
+          }}
+        >
+          -
+        </button>
+        <input
+          type="text"
+          aria-label={`item-${id}`}
+          className={`item-${id}`}
+          value={item}
+          disabled
+        ></input>
         <button
-        aria-label={`add-button-${id}`}
-        onClick={()=>{
-          // TODO: answer here
-        }}
-        >+</button>
+          aria-label={`add-button-${id}`}
+          onClick={() => {
+            // TODO: answer here
+            setItem(item + 1);
+          }}
+        >
+          +
+        </button>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 function App() {
-  //Add state for total 
+  //Add state for total
   // TODO: answer here
- 
+  const [total, setTotal] = React.useState(0);
+
   const dataDummy = [
     {
-      id    : '1',
-      image : "https://picsum.photos/id/0/150/",
-      title : "Laptop",
+      id: "1",
+      image: "https://picsum.photos/id/0/150/",
+      title: "Laptop",
     },
     {
-      id    : '2',
-      image : "https://picsum.photos/id/1010/150/",
-      title : "Buku",
+      id: "2",
+      image: "https://picsum.photos/id/1010/150/",
+      title: "Buku",
     },
     {
-      id    : '3',
-      image : "https://picsum.photos/id/103/150/",
-      title : "Sepatu",
-    }
+      id: "3",
+      image: "https://picsum.photos/id/103/150/",
+      title: "Sepatu",
+    },
   ];
 
   const showAlert = (isShow) => {
-    if(isShow){
+    if (isShow) {
       alert("Ups, udah kelebihan yaa");
     }
-  }
+  };
 
   return (
     <div className="state-props-2" aria-label="AppRoot">
       <h3>Keranjang Belanja</h3>
-      <div className='box-container'>
-          {dataDummy.map((element, index) => (
-            // TODO: answer here
-          ))}
+      <div className="box-container">
+        {dataDummy.map((element, index) => (
+          // TODO: answer here
+          <Item
+            key={index}
+            id={element.id}
+            image={element.image}
+            title={element.title}
+          />
+        ))}
       </div>
-      <div className='end-section'>
-          <h4>Kamu udah masukin&nbsp;
-            <span className='total-item' aria-label="total-item">{total}
-            </span>&nbsp;
-          barang</h4>
+      <div className="end-section">
+        <h4>
+          Kamu udah masukin&nbsp;
+          <span className="total-item" aria-label="total-item">
+            {total}
+          </span>
+          &nbsp; barang
+        </h4>
       </div>
     </div>
   );
