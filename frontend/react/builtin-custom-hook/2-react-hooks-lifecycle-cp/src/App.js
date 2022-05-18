8dc2b4ca65fdf8247cccc29a367b1b1e55f90ba3
faizalajiw <faizalajiwibowo7@gmail.com>
2022-05-18 10:41:11 +0700

    belum fix

--- frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
+++ frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
@@ -6,8 +6,16 @@ export default function App() {
 
 export function RandomQuote() {
   // TODO: answer here
+  const [quote, setQuote] = React.useState("");
+  // const [author, setAuthor] = React.useState("");
+  const [color, setColor] = React.useState("#fff");
+
   React.useEffect(() => {
     // TODO: answer here
+    const interval = setInterval(() => {
+      setColor(generateColor());
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
 
   function getQuote() {
@@ -19,4 +27,22 @@ export function RandomQuote() {
   }
 
   // TODO: answer here
+  function generateColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  return (
+    <div style={{ backgroundColor: color, width: "100vw", height: "100vh" }}>
+      <h1>Random Quote</h1>
+      <button onClick={getQuote}>Get Quote</button>
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <>
+          <p>{quote.content}</p>
+          <p>{quote.author}</p>
+        </>
+      )}
+    </div>
+  );
+
 }
