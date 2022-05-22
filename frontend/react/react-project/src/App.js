85f99a18941419dba46b7914b7ed7b3a8757268e
faizalajiw <faizalajiwibowo7@gmail.com>
2022-05-22 16:10:28 +0700

    App.js

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,14 +1,21 @@
-import "./App.css"
+import "./App.css";
 // TODO: answer here
-import React from "react" 
+import React from "react";
+import Navbar from "./components/Navbar";
+import LikeDislikeButton from "./components/LikeDislikeButton";
 
 function App() {
   // TODO: answer here
   return (
-    <div aria-label="App">
-      <h1 aria-label="App Title">Assignment React</h1>
+    <div aria-label="App" className="App">
+      <h1 aria-label="App Title">IG Clone</h1>
+
+      <div className="ImportItem">
+        <Navbar />
+        <LikeDislikeButton />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
