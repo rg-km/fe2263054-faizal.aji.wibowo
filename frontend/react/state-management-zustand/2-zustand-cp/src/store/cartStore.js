93bf4ae040b56d197e896d65b078650f60d455d7
faizalajiw <faizalajiwibowo7@gmail.com>
2022-05-26 18:30:23 +0700

    submit zustand 2

--- frontend/react/state-management-zustand/2-zustand-cp/src/store/cartStore.js
+++ frontend/react/state-management-zustand/2-zustand-cp/src/store/cartStore.js
@@ -1,17 +1,70 @@
-import create from "zustand"
+import create from "zustand";
 // TODO: answer here
+import { persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 const cartQuantityMiddleware = (config) => (set, get, api) =>
   config(
     // TODO: answer here
-    ,
+    (...args) => {
+      set(...args);
+      const moreThanStockItem = get().items.filter(
+        (item) => item.quantity > item.stock
+      );
+      const lessThanZeroItem = get().items.filter((item) => item.quantity < 1);
+      if (moreThanStockItem.length > 0) {
+        return set((state) => ({
+          items: state.items.map((item) =>
+            item.id === moreThanStockItem[0].id
+              ? { ...item, quantity: item.stock }
+              : item
+          ),
+        }));
+      }
+      if (lessThanZeroItem.length > 0) {
+        return set((state) => ({
+          items: state.items.map((item) =>
+            item.id === lessThanZeroItem[0].id ? { ...item, quantity: 1 } : item
+          ),
+        }));
+      }
+    },
     get,
-    api,
-  )
+    api
+  );
 
-const useCartStore = 
-// TODO: answer here
+const useCartStore =
+  // TODO: answer here
+  create(
+    persist(
+      immer(
+        cartQuantityMiddleware((set, get) => ({
+          items: [],
+          addItem: (item) => {
+            const itemExists = get().items.find((i) => i.id === item.id);
+            set((state) => ({
+              items: [
+                ...state.items.filter((i) => i.id !== item.id),
+                { ...item, quantity: itemExists ? itemExists.quantity + 1 : 1 },
+              ],
+            }));
+          },
+          removeItem: (itemId) => {
+            set((state) => ({
+              items: state.items.filter((i) => i.id !== itemId),
+            }));
+          },
+          changeQuantity: (itemId, quantity) => {
+            set((state) => ({
+              items: state.items.map((item) =>
+                item.id === itemId ? { ...item, quantity } : item
+              ),
+            }));
+          },
+        }))
+      ),
+      { name: "cart" }
+    )
+  );
 
-
-
-export default useCartStore
+export default useCartStore;
